622f8d922082dc61e9acc87207f3419bd311b8b6
lakshmanarao <lakshmanarao.pokuri121@gmail.com>
2024-06-17 11:17:08 -0500

    make use of postgresql

--- blogger_website/app/__init__.py
+++ blogger_website/app/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flaskuser:flaskpassword@db/flaskdb'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
