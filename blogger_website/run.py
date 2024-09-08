e31a9b40a116344cd43e1473a401a853dda38295
lakshmanarao <lakshmanarao.pokuri121@gmail.com>
2024-09-08 04:28:51 +0000

    delete previous version of blogger website

--- blogger_website/run.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-from flask_migrate import Migrate
-
-# Initialize Flask-Migrate
-migrate = Migrate(app, db)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
