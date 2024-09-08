eae567b5225229db04c0922f487ced8ffd4e5037
lakshmanarao <lakshmanarao.pokuri121@gmail.com>
2024-09-08 04:27:52 +0000

    update admin dashboard inside blogger website

---
+++ blogger_web/app/routes.py
@@ -0,0 +1,159 @@
+from flask import render_template, url_for, flash, redirect, request, current_app, abort
+from app import app, db, bcrypt
+import os, secrets
+from itsdangerous import TimedSerializer as Serializer
+from app.forms import RegistrationForm, LoginForm, PostForm, UpdateProfileForm, SearchForm
+from app.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+from functools import wraps
+from PIL import Image
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    # Create the directory if it doesn't exist
+    os.makedirs(os.path.dirname(picture_path), exist_ok=True)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+class LikeForm(FlaskForm):
+    submit = SubmitField('Like')
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_admin:
+            abort(403)
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route("/")
+@app.route("/home")
+def home():
+    
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.paginate(page=page, per_page=5)
+    
+    return render_template('index.html', posts=posts)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        email = User.query.filter_by(email=form.email.data).first()
+        if user or email:
+            flash('Username or Email already exists.', 'danger')
+        else:
+            hashed_password = generate_password_hash(form.password.data)
+            is_first_user = User.query.count() == 0
+            user = User(username=form.username.data, email=form.email.data, password=hashed_password, is_admin=is_first_user)
+            db.session.add(user)
+            db.session.commit()
+            flash('Your account has been created! You are now able to log in', 'success')
+            return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user, tags=form.tags.data)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('new_post.html', title='New Post', form=form)
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/like", methods=['POST'])
+@login_required
+def like_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if current_user in post.liked_by:
+        post.liked_by.remove(current_user)
+        db.session.commit()
+        flash('You unliked the post!', 'success')
+    else:
+        post.liked_by.append(current_user)
+        db.session.commit()
+        flash('You liked the post!', 'success')
+    return redirect(url_for('home'))
+
+@app.route("/profile", methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = UpdateProfileForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('profile.html', title='Profile', image_file=image_file, form=form)
+
+
+@app.route("/admin_dashboard")
+@login_required
+@admin_required
+def admin_dashboard():
+    page = request.args.get('page', 1, type=int)
+    users = User.query.paginate(page=page, per_page=10)
+    post_page = request.args.get('post_page', 1, type=int)
+    posts = Post.query.paginate(page=post_page, per_page=10)
+    return render_template('admin_dashboardv2.html', users=users, posts=posts)
+
+@app.route("/search", methods=['GET'])
+def search():
+    query = request.args.get('q', '')
+    # Implement your search logic here, for example, searching posts
+    results = Post.query.filter(Post.title.contains(query) | Post.content.contains(query)).all()
+    return render_template('search_results.html', query=query, results=results)
+
